8c6ebbb033930e58add43c31dc391e3f71ec7afa
dvaustria741-a11y <dvaustria741@gmail.com>
2026-06-29 16:00:41 +0800

    feat: add src/main/java/com/auraclient/module/misc/FastPlace.java

---
+++ src/main/java/com/auraclient/module/misc/FastPlace.java
@@ -0,0 +1,16 @@
+package com.auraclient.module.misc;
+
+import com.auraclient.module.Module;
+import net.minecraft.client.MinecraftClient;
+
+public class FastPlace extends Module {
+    public FastPlace() {
+        super("FastPlace", "Removes block/item placement delay.", Category.MISC);
+    }
+
+    @Override
+    public void onTick(MinecraftClient client) {
+        if (client.player == null) return;
+        if (client.itemUseCooldown > 0) client.itemUseCooldown = 0;
+    }
+}
